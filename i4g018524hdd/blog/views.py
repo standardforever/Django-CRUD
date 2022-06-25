f53f2c0cbefde739ccf46fde2d1ad0875bacd99f
standardforever <standard.forever123@gmail.com>
2022-06-25 01:12:32 -0400

    updated

---
+++ i4g018524hdd/blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from .models import Post
+
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name= "post_list.html"
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name="post_form.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name="post_detail.html"
+    
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name="post_form.html"
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name="post_confirm_delete.html"
